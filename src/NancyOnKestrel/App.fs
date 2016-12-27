71122e6e90a4e4422e3b8346b4258322bd4afe19
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2016-12-27 12:27:04 -0500

    Cleanup NancyOnKestrel

--- src/NancyOnKestrel/App.fs
+++
@@ -1,6 +0,0 @@
-module App
-open Nancy
-type HelloWorldModule() as this =
-    inherit NancyModule()
-    do
-        this.Get("/", fun _ -> "Hello From Nancy on Kestrel!" :> obj )
--- src/NancyOnKestrel/Program.fs
+++ src/NancyOnKestrel/Program.fs
@@ -1,22 +1,31 @@
-﻿module Main
+﻿namespace App 
 
-open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Configuration
-open Nancy.Owin
+module App =
+    open Nancy
+    type HelloWorldModule() as this =
+        inherit NancyModule()
+        do
+            this.Get("/", fun _ -> "Hello From Nancy on Kestrel!" :> obj )
 
-type Startup() =
-    member this.Configure (app : IApplicationBuilder) : unit=
-        app.UseOwin(fun o -> o.UseNancy() |> ignore) |> ignore
+module Main =
 
-[<EntryPoint>]
-let main argv =
-    WebHostBuilder()
-        .UseUrls("http://localhost:8083")
-        .UseKestrel()
-        .UseStartup<Startup>()
-        .Build()
-        .Run()
+    open Microsoft.AspNetCore.Builder
+    open Microsoft.AspNetCore.Hosting
+    open Microsoft.Extensions.Configuration
+    open Nancy.Owin
+
+    type Startup() =
+        member this.Configure (app : IApplicationBuilder) : unit=
+            app.UseOwin(fun o -> o.UseNancy() |> ignore) |> ignore
+
+    [<EntryPoint>]
+    let main argv =
+        WebHostBuilder()
+            .UseUrls("http://localhost:8083")
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .Build()
+            .Run()
 
 
-    0 // return an integer exit code
+        0    // return an integer exit code
