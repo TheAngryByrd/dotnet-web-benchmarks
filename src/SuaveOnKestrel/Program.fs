4cb050297b5b7dfa2a75138f58b38d0db4b67430
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2016-12-29 07:29:41 -0500

    Make suave use routing in kestrel

--- src/SuaveOnKestrel/Program.fs
+++ src/SuaveOnKestrel/Program.fs
@@ -4,19 +4,18 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
 open Suave
+open Suave.Http
 open Suave.Operators
 open Suave.Successful
 open Suave.AspNetCore
 
 module App =
-    let catchAll =
-        fun (ctx : HttpContext) ->
-            OK "Hello world from Suave on Kestrel" 
-            <| ctx
+    open Suave.Filters
+    let suaveApp = GET >=> path "/" >=> OK "Hello world from Suave on Kestrel" 
 
     type Startup () =
         member __.Configure (app : IApplicationBuilder) =
-            app.UseSuave(catchAll) |> ignore
+            app.UseSuave(suaveApp) |> ignore
 
     [<EntryPoint>]
     let main _ =
