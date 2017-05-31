c644e583d0a1ab05f416da6b784b80a505844be3
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2017-05-30 23:08:55 -0400

    Added Freya and Freya.Hopac across servers

---
+++ src/Katana/Freya.Hopac/Program.fs
@@ -0,0 +1,40 @@
+﻿module FreyaOnMono
+open System
+open System.Threading
+open Freya.Core
+open Freya.Machines.Http
+open Freya.Routers.Uri.Template
+
+
+let hello =
+    freya {
+
+        return Represent.text ("Hello World!") }
+
+let machine =
+    freyaMachine {
+        handleOk hello }
+
+let router =
+    freyaRouter {
+        resource "/" machine }
+
+type HelloWorld () =
+    member __.Configuration () =
+        OwinAppFunc.ofFreya (router)
+
+open System
+open Microsoft.Owin.Hosting
+
+[<EntryPoint>]
+let main _ =
+
+    let url = "http://127.0.0.1:8083"
+    printfn "Listening on %s" url
+    let _ = WebApp.Start<HelloWorld> (url)
+    let quitEvent = new ManualResetEvent(false)
+    Console.CancelKeyPress.Add(fun (args) -> 
+        quitEvent.Set() |> ignore
+        args.Cancel <- true)
+    quitEvent.WaitOne() |> ignore
+    0
---
+++ src/Kestrel/Freya.Hopac/Program.fs
@@ -0,0 +1,35 @@
+﻿module Main
+
+open Freya.Core
+open Freya.Machines.Http
+open Freya.Routers.Uri.Template
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open KestrelInterop
+
+
+let hello =
+    freya {
+        return Represent.text ("Hello World!") }
+
+let machine =
+    freyaMachine {
+        handleOk hello }
+
+let router =
+    freyaRouter {
+        resource "/" machine }
+
+let root = UriTemplateRouter.Freya router
+[<EntryPoint>]
+let main argv =
+  let configureApp =
+    ApplicationBuilder.useFreya root
+
+  WebHost.create ()
+  |> WebHost.bindTo [|"http://*:8083"|]
+  |> WebHost.configure configureApp
+  |> WebHost.buildAndRun
+
+  0 // return an integer exit code
